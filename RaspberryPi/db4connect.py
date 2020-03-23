5c76a28869f1fa8438e992079d7dce06283fff5f
priyanshbendre <priyanshbendre@gmail.com>
2020-03-22 20:30:11 -0700

    file for database connectivity test

--- RaspberryPi/db4connect.py
+++ RaspberryPi/db4connect.py
@@ -1,8 +1,20 @@
-import mysql.connector as mysql
+import pymysql.cursors
 
-db = mysql.connect(
-	host = "db4free.net",
-	user = "parkway",
-	passwd = "capstone"
-	)
-print(db)
+connection = pymysql.connect(host='db4free.net',
+                             user='parkway',
+                             password='capstone',
+                             db='db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM parkway.new_table"
+        #cursor.execute(sql, (2,'Bendre'))
+        result = cursor.fetchone()
+        print(result)
+
+    connection.commit()
+finally:
+    connection.close()
