f0a017a17f84f64f5fc2ed04626a6ad10b8295f0
priyanshbendre <priyanshbendre@gmail.com>
2020-05-01 00:24:57 -0700

    working code with RDS

--- RaspberryPi/test_manual.py
+++ RaspberryPi/test_manual.py
@@ -1,18 +1,17 @@
-from gpiozero import DistanceSensor as distsense
 from time import sleep
 import mysql.connector #install mysql package on raspberry pi
 
 if __name__ =="__main__":
-    sensor = distsense(trigger = 18, echo = 24)
+    # sensor = distsense(trigger = 18, echo = 24)
     dis_vec = [False]*6 
 
 #MySQL initialization
 
     mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
+    host="testdb.cooervgj57jr.us-west-1.rds.amazonaws.com",
+    user="admin",
     passwd="admin123",
-    database="test_newdb"
+    database="test123"
     )
     mycursor = mydb.cursor()
 
@@ -20,19 +19,21 @@ if __name__ =="__main__":
 
     while True:
         sleep(2)
-        distance = round((sensor.distance*100),2)
+        # distance = round((sensor.distance*100),2)
         #print ("Distance: {}cm ".format(distance), end = ' ') #check why this is printing together
-        
+        distance = float(input("enter distance: "))
         check = sum(dis_vec)
         if sum(dis_vec)==5 and distance <100:
-        	print("Change status to occupied") #run update into query
-            #mycursor.execute("INSERT INTO new_table (id, mobile) VALUES ('12',84735);")
-            #mydb.commit()
+            print("Change status to occupied") #run update into query
+            mycursor.execute("INSERT INTO test456 (status) VALUES (0)")
+            mydb.commit()
 
         if sum(dis_vec)==1 and distance>100:
-        	print("Change status to unoccupied")
-            #mycursor.execute("INSERT INTO new_table (id, mobile) VALUES ('12',84735);")
-            #mydb.commit()
+            sql= "Insert INTO test456 (status) values (%s)"
+            val=(1)
+            mycursor.execute(sql,val)
+            mydb.connection.commit()
+            print("Change status to unoccupied")
 
         if distance <100 :
             dis_vec.append(True)
